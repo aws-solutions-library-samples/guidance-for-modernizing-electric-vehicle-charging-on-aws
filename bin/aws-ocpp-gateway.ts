5756d5cb7170117923c78656af17d38bbceba282
Bigad Soleiman <83646217+bigadsoleiman@users.noreply.github.com>
2023-04-27 11:33:36 +0100

    Leave default chip architecture to ARM

--- bin/aws-ocpp-gateway.ts
+++ bin/aws-ocpp-gateway.ts
@@ -13,7 +13,7 @@ new AwsOcppGatewayStack(app, 'AwsOcppGatewayStack', {
 
   // The default chip architecture used for ECS is ARM
   // You can override it to use X86_64 by uncommenting the following line
-  architecture: 'X86_64',
+  // architecture: 'X86_64',
 
   // If you have a hosted zone in Route53, you can uncomment the following line
   // and replace "yourdomain.com" with your domain name
